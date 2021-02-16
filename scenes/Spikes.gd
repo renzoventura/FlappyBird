84546d828125686f0a182e15e7e77b9ebe2e79a8
renzoventura <edrickventura1996@gmail.com>
2021-02-16 23:22:20 +1300

    Implement dynamic hazards

--- scenes/Spikes.gd
+++ scenes/Spikes.gd
@@ -3,9 +3,17 @@ extends Node2D
 
 var SPEED = 5;
 var motion = Vector2(0,0)
+var gap_sizes = [900, 1000, 850]
+var random_centers = [100, 200, 300, 350]
+var current_gap_size;
+onready var topSpike = $TopSpike;
+onready var bottomSpike = $BottomSpike;
 
 func _ready():
-	print(position)
+	var current_gap_size = gap_sizes[randi() % gap_sizes.size()]
+	topSpike.position.y = topSpike.position.y + (current_gap_size/2)
+	bottomSpike.position.y = bottomSpike.position.y - (current_gap_size/2)
+	position.y = random_centers[randi() % random_centers.size()]
 	pass 
 	
 func _process(delta):
@@ -13,8 +21,3 @@ func _process(delta):
 
 func move():
 	position.x -= SPEED
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
